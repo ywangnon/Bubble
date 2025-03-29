8d220467bf6c255dd497ce83d764756adb932c68
ywangnon <ywangnon@hanmail.net>
2025-03-30 01:39:57 +0900

    refactor: restructure bubble animation
    상황
    기존에는 BubbleView가 실제로 사용되지 않음.
    동그란 거품만으로는 어색한 느낌이 듦
    
    결과만이 아닌 사용 편의성을 높이기 위한 작업을 위해 코드 정리

--- Bubble/BubbleView.swift
+++ Bubble/BubbleView.swift
@@ -9,38 +9,38 @@ import UIKit
 
 class BubbleView: UIView {
     override func draw(_ rect: CGRect) {
-        let circlePath = UIBezierPath(roundedRect: rect,
-                                      cornerRadius: self.bounds.height/2)
+        let circlePath = UIBezierPath(ovalIn: rect)
         
-        UIColor.black.set()
-        circlePath.stroke()
-        UIColor.systemRed.set()
+        // 색상
+        UIColor.systemRed.setFill()
         circlePath.fill()
         
-        let lightPath = UIBezierPath(arcCenter: CGPoint(x: self.bounds.midX, y: self.bounds.midY),
-                                     radius: ((self.bounds.height/2)-8),
-                                     startAngle: (180 * .pi) / 180,
-                                     endAngle: (270 * .pi) / 180,
-                                     clockwise: true)
-        UIColor.black.set()
+        // 테두리
+        UIColor.black.setStroke()
+        circlePath.stroke()
+        
+        // 광택 효과(arc)
+        let lightPath = UIBezierPath(
+            arcCenter: CGPoint(x: bounds.midX, y: bounds.midY),
+            radius: (bounds.height / 2 - 8),
+            startAngle: .pi,
+            endAngle: 1.5 * .pi,
+            clockwise: true
+        )
+        UIColor.black.setStroke()
         lightPath.stroke()
     }
     
-    // 코드로 view 인스턴스를 생성하기 위한 이니셜라이저
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.backgroundColor = .clear
         
-        // 테두리 확인용 색
-        self.backgroundColor = .yellow
-        
-        // 테두리 자르기
-        self.layer.cornerRadius = self.bounds.height/2
-        print("버블뷰 생성", self.bounds.height/2)
+        // 둥근 모서리(클리핑)
+        // 꼭 init이 아니라 layoutSubviews에서 갱신해도 됨
+        self.layer.cornerRadius = frame.height / 2
         self.layer.masksToBounds = true
     }
     
-    // 인터페이스빌더, 스토리보드를 이용해 view를 생성할때 코드로 변환해주는 언아카이빙 과정이 필요한데, 이를 해주는 것이 required init
-    // 결국에는 코드로 되어있어야 컴파일을 할 때 컴파일러가 인식할 수 있다.
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- Bubble/ViewController.swift
+++ Bubble/ViewController.swift
@@ -7,51 +7,65 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     
-    
-    
+    // 타이머를 프로퍼티로 들고 있어서 필요 시 invalidate 가능
+    private var bubbleTimer: Timer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .black
+        
+        // 화면 보일 때마다 계속 생성하고 싶다면 viewDidAppear에서도 가능
+        // 한 번만 시작할 거라면 viewDidLoad에서 시작
+        bubbleTimer = Timer.scheduledTimer(timeInterval: 0.05,
+                                           target: self,
+                                           selector: #selector(bubbleAnimation),
+                                           userInfo: nil,
+                                           repeats: true)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        // 일정 시간 간격으로 계속해서 생성
-        Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(bubbleAnimation), userInfo: nil, repeats: true)
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // 화면 사라질 때 타이머 정지
+        bubbleTimer?.invalidate()
+        bubbleTimer = nil
     }
     
     @objc func bubbleAnimation() {
-        // 버블뷰 생성
-        let xPosition = CGFloat.random(in: -100...self.view.bounds.width)
-        let yPosition = self.view.bounds.height - CGFloat.random(in: 100...200)
-        let bubbleView = UIImageView(frame: CGRect(origin: CGPoint(x: xPosition, y: yPosition), size: CGSize(width: 150, height: 150)))
-        bubbleView.image = UIImage(named: "bubble")
+        // 버블의 시작 위치와 크기 결정
+        let randomX = CGFloat.random(in: -100...self.view.bounds.width)
+        let randomY = self.view.bounds.height - CGFloat.random(in: 100...200)
+        let startSize: CGFloat = 150
+        let bubbleFrame = CGRect(x: randomX, y: randomY, width: startSize, height: startSize)
+        
+        // 커스텀 버블 뷰 생성
+        let bubbleView = BubbleView(frame: bubbleFrame)
+        // 반투명도
         bubbleView.alpha = 0.5
         self.view.addSubview(bubbleView)
         
-        UIView.animate(withDuration: 4, delay: 0, options: []) {
-            // 버블뷰 이동 & 흐려짐
-            UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.7) {
-                bubbleView.frame.origin.y -= CGFloat.random(in: 0...self.view.bounds.height/4)
-                
-                if Bool.random() {
-                    print("+x")
-                    bubbleView.frame.origin.x += CGFloat.random(in: 0...80)
-                } else {
-                    print("-x")
-                    bubbleView.frame.origin.x -= CGFloat.random(in: 0...80)
-                }
-                
-                bubbleView.frame.size = CGSize(width: 200, height: 200)
-                bubbleView.alpha = 0
+        // 애니메이션
+        UIView.animate(withDuration: 4.0, delay: 0, options: []) {
+            // y 좌표를 위로 올리기
+            bubbleView.frame.origin.y -= CGFloat.random(in: 0...self.view.bounds.height / 4)
+            
+            // x 좌표 랜덤 이동
+            if Bool.random() {
+                bubbleView.frame.origin.x += CGFloat.random(in: 0...80)
+            } else {
+                bubbleView.frame.origin.x -= CGFloat.random(in: 0...80)
             }
+            
+            bubbleView.frame.size = CGSize(width: 200, height: 200)
+            
+            // 서서히 사라지기
+            bubbleView.alpha = 0
         } completion: { _ in
-            // 버블뷰 삭제
+            // 끝나면 뷰 제거
             bubbleView.removeFromSuperview()
-            print("animation done")
         }
     }
 }
-
